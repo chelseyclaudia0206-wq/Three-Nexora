98bd11f29c3efa58e9072b154248b88f4d76fa7c
Chelsey Claudia <chelseyclaudia0206@gmail.com>
2026-06-27 16:50:37 +0700

    menambahkan file database

--- penitipan_hewan.sql
+++ penitipan_hewan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 24, 2026 at 05:13 PM
+-- Generation Time: Jun 27, 2026 at 11:49 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -41,7 +41,11 @@ CREATE TABLE `akun` (
 --
 
 INSERT INTO `akun` (`id_akun`, `nama_lengkap`, `username`, `password`, `no_hp`, `role`) VALUES
-(1, 'Admin Penitipan', 'admin', 'admin123', '08123456789', 'admin');
+(1, 'Admin Penitipan', 'admin', 'admin123', '08123456789', 'admin'),
+(3, 'Aya Lestari', 'ayalestari', 'aya123', '0811001100111', 'pemilik'),
+(5, 'Catherine', 'cath', 'cath123', '08110011001100', 'pemilik'),
+(6, 'Natasha Aqila', 'nata', 'nata123', '08999000111000', 'pemilik'),
+(7, 'Del Lana', 'lana', 'lana123', '081110004455', 'pemilik');
 
 -- --------------------------------------------------------
 
@@ -58,6 +62,16 @@ CREATE TABLE `hewan` (
   `catatan` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `hewan`
+--
+
+INSERT INTO `hewan` (`id_hewan`, `id_pemilik`, `nama_hewan`, `jenis_hewan`, `umur`, `catatan`) VALUES
+(4, 3, 'Milo', 'Kucing', 2, 'Tidak suka makanan basah'),
+(8, 5, 'mochi', 'Kucing', 2, 'alergi susu'),
+(9, 6, 'milky', 'Kelinci', 1, '-'),
+(10, 7, 'bleky', 'Anjing', 2, '-');
+
 -- --------------------------------------------------------
 
 --
@@ -66,11 +80,20 @@ CREATE TABLE `hewan` (
 
 CREATE TABLE `perawatan` (
   `id_perawatan` int(11) NOT NULL,
-  `id_reservasi` int(11) NOT NULL,
-  `jenis_perawatan` varchar(50) NOT NULL,
+  `jenis_hewan` varchar(50) NOT NULL,
   `tanggal_perawatan` date NOT NULL,
-  `keterangan` text DEFAULT NULL
+  `jenis_perawatan` varchar(100) NOT NULL,
+  `keterangan` text DEFAULT NULL,
+  `status` varchar(30) DEFAULT 'Belum Selesai'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `perawatan`
+--
+
+INSERT INTO `perawatan` (`id_perawatan`, `jenis_hewan`, `tanggal_perawatan`, `jenis_perawatan`, `keterangan`, `status`) VALUES
+(4, 'Kelinci', '2026-06-28', 'Makan', '-', 'Selesai'),
+(5, 'Kelinci', '2026-06-30', 'Grooming', '-', 'Belum Selesai');
 
 -- --------------------------------------------------------
 
@@ -80,13 +103,22 @@ CREATE TABLE `perawatan` (
 
 CREATE TABLE `reservasi` (
   `id_reservasi` int(11) NOT NULL,
-  `id_hewan` int(11) NOT NULL,
+  `jenis_hewan` varchar(50) NOT NULL,
   `tanggal_checkin` date NOT NULL,
   `tanggal_checkout` date NOT NULL,
   `lama_hari` int(11) NOT NULL,
+  `harga_per_hari` int(11) NOT NULL,
   `total_biaya` int(11) NOT NULL,
   `status` varchar(30) DEFAULT 'Menunggu'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `reservasi`
+--
+
+INSERT INTO `reservasi` (`id_reservasi`, `jenis_hewan`, `tanggal_checkin`, `tanggal_checkout`, `lama_hari`, `harga_per_hari`, `total_biaya`, `status`) VALUES
+(4, 'Kelinci', '2026-06-21', '2026-06-24', 3, 40000, 120000, 'Aktif'),
+(5, 'Kucing', '2026-06-28', '2026-06-29', 1, 50000, 50000, 'Menunggu');
 
 --
 -- Indexes for dumped tables
@@ -110,15 +142,13 @@ ALTER TABLE `hewan`
 -- Indexes for table `perawatan`
 --
 ALTER TABLE `perawatan`
-  ADD PRIMARY KEY (`id_perawatan`),
-  ADD KEY `id_reservasi` (`id_reservasi`);
+  ADD PRIMARY KEY (`id_perawatan`);
 
 --
 -- Indexes for table `reservasi`
 --
 ALTER TABLE `reservasi`
-  ADD PRIMARY KEY (`id_reservasi`),
-  ADD KEY `id_hewan` (`id_hewan`);
+  ADD PRIMARY KEY (`id_reservasi`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -128,25 +158,25 @@ ALTER TABLE `reservasi`
 -- AUTO_INCREMENT for table `akun`
 --
 ALTER TABLE `akun`
-  MODIFY `id_akun` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_akun` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `hewan`
 --
 ALTER TABLE `hewan`
-  MODIFY `id_hewan` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_hewan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `perawatan`
 --
 ALTER TABLE `perawatan`
-  MODIFY `id_perawatan` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_perawatan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `reservasi`
 --
 ALTER TABLE `reservasi`
-  MODIFY `id_reservasi` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_reservasi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Constraints for dumped tables
@@ -157,18 +187,6 @@ ALTER TABLE `reservasi`
 --
 ALTER TABLE `hewan`
   ADD CONSTRAINT `hewan_ibfk_1` FOREIGN KEY (`id_pemilik`) REFERENCES `akun` (`id_akun`);
-
---
--- Constraints for table `perawatan`
---
-ALTER TABLE `perawatan`
-  ADD CONSTRAINT `perawatan_ibfk_1` FOREIGN KEY (`id_reservasi`) REFERENCES `reservasi` (`id_reservasi`);
-
---
--- Constraints for table `reservasi`
---
-ALTER TABLE `reservasi`
-  ADD CONSTRAINT `reservasi_ibfk_1` FOREIGN KEY (`id_hewan`) REFERENCES `hewan` (`id_hewan`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
